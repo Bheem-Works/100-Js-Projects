07509668ef61a722d29e03360d1517f9edf1e7aa
Bheem-Works <shokokomichan098@gmail.com>
2025-03-29 23:23:16 +0545

    socialNavbar commit

---
+++ social-media/social.js
@@ -0,0 +1,17 @@
+const menuEl = document.querySelector(".menu");
+const menuTextel = document.querySelector(".menu p");
+const socialListsEl = document.querySelector(".social-lists");
+const liEls = document.querySelectorAll(".social-lists li");
+
+menuEl.addEventListener("click", () => {
+  socialListsEl.classList.toggle("hide");
+  menuEl.classList.toggle("rotate");
+});
+
+liEls.forEach((liEl) => {
+  liEl.addEventListener("click", () => {
+    menuTextel.innerHTML = liEl.innerHTML; // Fixed: innerHTML instead of innerHtml
+    socialListsEl.classList.add("hide");
+    menuEl.classList.toggle("rotate");
+  });
+});
