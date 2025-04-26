f32335d71824355aa7c517102bdb28350355892c
Bhim magar <shokokomichan098@gmail.com>
2025-04-26 10:44:18 +0545

    searchButton

--- autoWrite/auto.js
+++ autoWrite/auto.js
@@ -1,13 +1,28 @@
-let textStore = "Miso is beautiful";
-let store = document.getElementById('demo');
-let index = 0;
-function autoWrite () {
-    store.innerText = textStore.slice(0,index);
-    index++;
+const containerEl = document.querySelector(".contianer");
 
-    if(index > textStore.length) {
-        index = 0;
-    }
+const careers = ["YouTuber", "Web Developer", "Freelancer", "Instructor"];
+
+let careerIndex = 0;
+
+let characterIndex = 0;
+
+updateText();
+
+function updateText() {
+  characterIndex++;
+  containerEl.innerHTML = `
+    <h1>I am ${careers[careerIndex].slice(0, 1) === "I" ? "an" : "a"} ${careers[
+    careerIndex
+  ].slice(0, characterIndex)}</h1>
+    `;
+
+  if (characterIndex === careers[careerIndex].length) {
+    careerIndex++;
+    characterIndex = 0;
+  }
+
+  if (careerIndex === careers.length) {
+    careerIndex = 0;
+  }
+  setTimeout(updateText, 400);
 }
-
-setInterval(autoWrite,300);
