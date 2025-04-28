29d4c1efb459995e94a3c3bd595e30bd50017836
Bhim magar <shokokomichan098@gmail.com>
2025-04-28 15:51:30 +0545

    TestAutoJs

--- Carousel/Carousel.js
+++ Carousel/Carousel.js
@@ -8,26 +8,33 @@ let timeOut;
 
 nextEl.addEventListener("click",()=>{
     currentImg++;
-    clearTimeout(timeOut);
     updateImg();
+    clearTimeout(timeOut);
 })
 
 prevEl.addEventListener("click",()=>{
     currentImg--;
-    clearTimeout(timeOut);
     updateImg();
+    clearTimeout(timeOut);
 })
 
 updateImg();
 function updateImg() {
+    console.log("currentImg",currentImg);
+    console.log("imgEl.length",imgEl.length);
     if(currentImg > imgEl.length) {
         currentImg = 1;
+        // console.log("if is running")
     } else if (currentImg < 1) {
         currentImg = imgEl.length;
+        console.log("else is running")
     }
     
     console.log("this is the current image",currentImg);
-    imgGalleryEl.style.transform = `translateX(-${(currentImg-1)*500}px)`;
+    
+    const imageStyle = imgGalleryEl.style.transform = `translateX(-${(currentImg-1)*500}px)`;
+    console.log(imageStyle);
+    
     timeOut = setTimeout(()=>{
         currentImg++;
         updateImg();
--- autoWrite/auto.js
+++ autoWrite/auto.js
@@ -3,26 +3,31 @@ const containerEl = document.querySelector(".contianer");
 const careers = ["YouTuber", "Web Developer", "Freelancer", "Instructor"];
 
 let careerIndex = 0;
-
 let characterIndex = 0;
 
 updateText();
 
 function updateText() {
   characterIndex++;
+
+  console.log(`careerIndex: ${careerIndex}, characterIndex: ${characterIndex}`);
+
   containerEl.innerHTML = `
     <h1>I am ${careers[careerIndex].slice(0, 1) === "I" ? "an" : "a"} ${careers[
     careerIndex
   ].slice(0, characterIndex)}</h1>
-    `;
+  `;
 
   if (characterIndex === careers[careerIndex].length) {
+    console.log("One career finished, switching to next career!");
     careerIndex++;
     characterIndex = 0;
   }
 
   if (careerIndex === careers.length) {
+    console.log("All careers finished, restarting from first!");
     careerIndex = 0;
   }
+
   setTimeout(updateText, 400);
 }
