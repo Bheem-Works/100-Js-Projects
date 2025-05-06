0dd44cbe9c7021b85416e690c4a4a768e54dedbd
Bhim magar <shokokomichan098@gmail.com>
2025-05-07 00:47:05 +0545

    binaryCommit

--- binaryConver/binary.js
+++ binaryConver/binary.js
@@ -1,2 +1,25 @@
-// varaibles; 
-const binaryCode
+const binaryInput = document.getElementById('binaryCode');      // user enters binary (1010)
+const textInput = document.getElementById('textInput');          // user enters normal text (Hello)
+const textOutput = document.getElementById('textOutput');        // shows the decoded binary
+const binaryOutput = document.getElementById('binaryOutput');    // shows binary of entered text
+
+// binary to text;
+binaryInput.addEventListener('input',()=>{
+    let binaryValue = binaryInput.value.trim();
+    let binary = binaryValue
+    .split('')
+    .map(b=>String.
+    fromCharCode(parseInt(b,2)))
+    .join('');
+    textOutput.innerText = binary || "BinaryValue";
+})
+
+
+// text to binary;
+textOutput.addEventListener('input',()=>{
+    let textValue = textOutput.value;
+    let binaryNumber = [...textValue]
+    .map(char => char.charCodeAt(0).toString(2))
+    .join(' ');
+    binaryOutput.innerText = binaryNumber || "TextValue";
+})
