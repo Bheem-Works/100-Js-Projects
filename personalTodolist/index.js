3bddc76e6c6e2b6a64b0ea176b8cbbd49c0c4023
bheem-works <shokokomichan098@gmail.com>
2025-06-25 20:14:21 +0545

    PushUnChanged

--- personalTodolist/index.js
+++ personalTodolist/index.js
@@ -1,21 +1,12 @@
-// This is the personalTodoListTabjk;
-
-// I have to create the varaibles;
-
-const buttonClick = document.querySelector(".forButtons");
-buttonClick.addEventListener("click", add);
+document.querySelector(".addButton").addEventListener("click", add);
 
 function add() {
-  const input_element = document.getElementById("inputTest").value;
-
-  const input_checkBox = document.createElement("input");
-  input_checkBox.type = "checkbox";
-  input_checkBox.classList.add("inputChecBox");
-  const mix = document.querySelector(".mix");
-  mix.appendChild(input_checkBox);
-
-  // creting the elements;
-  const p_element = document.createElement("p");
-  p_element.innerHTML = input_element;
-  mix.appendChild(p_element);
+  // take the input value;
+  const inputValue = document.getElementById("inputTest").value;
+  // creating the p elements;
+  const pElement = document.createElement("p");
+  pElement.textContent = inputValue;
+  const addP_append = pElement.appendChild(inputValue);
+  const appendToBody = document.querySelector(".mix");
+  appendToBody.appendChild(addP_append);
 }
